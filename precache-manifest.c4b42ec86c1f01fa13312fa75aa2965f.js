a4108cbd5171db9fcc7b62b302a9dbc411573882
kiminohero <bishal5058prime@gmail.com>
2019-11-02 12:24:55 +0530

    Updates

---
+++ precache-manifest.c4b42ec86c1f01fa13312fa75aa2965f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cc64d144bd68cccecb3b86ce532ee4",
+    "url": "/Random-Number/index.html"
+  },
+  {
+    "revision": "86ae3293bfb1275df5e3",
+    "url": "/Random-Number/static/js/2.e7b522f9.chunk.js"
+  },
+  {
+    "revision": "c7cb9639c10ca663492e",
+    "url": "/Random-Number/static/js/main.0a6a6c7b.chunk.js"
+  },
+  {
+    "revision": "fde4b86bf6d122dbab7e",
+    "url": "/Random-Number/static/js/runtime-main.2d960523.js"
+  }
+]);
